48502bb35a36a01701fe3895d82fb4b4ffbdf7b2
Angelica <Angelica137@users.noreply.github.com>
2020-04-11 22:32:28 +0100

    add bookmark location for list of bookmarks, test fail

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,6 +1,11 @@
 feature 'View bookmark list' do
-  scenario 'a user sees their bookmarks when visiting the bookmark list' do
+  scenario 'visiting the index page' do
     visit ('/')
-    expect(page).to have_content "Bookmark Manager"
+    expect(page).to have_content 'Bookmark Manager'
+  end
+
+  scenario 'requesting to see the list of bookmars' do
+    visit ('/bookmarks')
+    expect(page).to have_content 'These are your bookmarks :)'
   end
 end
