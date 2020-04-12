c25577b32411f4ae6c26a343d744d0a5a4608995
Angelica <Angelica137@users.noreply.github.com>
2020-04-12 10:09:56 +0100

    reafctor controller content to view files

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Bookmark Manager'
+    erb :index
   end
 
   get '/bookmarks' do
-    'These are your bookmarks :)'
+    erb :'bookmarks/view_bookmarks'
   end
 
   run! if app_file == $0
