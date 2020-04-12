7f22998d997586a286ae592f9730bf0ab378a0f8
Angelica <Angelica137@users.noreply.github.com>
2020-04-12 20:34:33 +0100

    refactor dummy data from tests

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -6,12 +6,9 @@ feature 'View bookmark list' do
 
   scenario 'requesting to see the list of bookmars' do
 
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    # Add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
 
     visit ('/bookmarks')
     expect(page).to have_content 'These are your bookmarks :)'
