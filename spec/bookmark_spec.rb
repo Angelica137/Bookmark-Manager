f446f423f3b693a7681da71710b8189235b4af62
Angelica <Angelica137@users.noreply.github.com>
2020-04-12 12:28:24 +0100

    add Bookmark class .all method unit test, fail

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,7 +3,7 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
-      bookmarks = Bookmarks.all
+      bookmarks = Bookmark.all
 
       expect(page).to have_content 'http://www.makersacademy.com'
       expect(page).to have_content 'http://www.destroyallsoftware.com'
