9d8648cdf9eb36ad90381acf3625ebd87cd0e2f3
Angelica <Angelica137@users.noreply.github.com>
2020-04-13 11:14:34 +0100

    wrap database output into an object

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,16 +3,19 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      Bookmark.create(url: "http://www.makersacademy.com", title: 'Makers Academy')
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: 'Makers Academy')
       Bookmark.create(url: "http://www.destroyallsoftware.com" , title: 'Destroy All Software')
       Bookmark.create(url: "http://www.google.com" , title: 'Google')
 
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include 'http://www.makersacademy.com'
-      expect(bookmarks).to include 'http://www.destroyallsoftware.com'
-      expect(bookmarks).to include 'http://www.google.com'
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(boookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
